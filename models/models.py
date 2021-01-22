f4244dba4ae6d8bf598ff0c14eef63b31269b8d3
Sungwon Hwang <shwang.14@kaist.ac.kr>
2021-01-22 17:51:09 +0900

    changed some footnotes

--- models/models.py
+++ models/models.py
@@ -34,6 +34,7 @@ class WN_GCN_layer(nn.Module):
         alpha = torch.nn.Parameter(torch.tensor(1/9), requires_grad=True).to('cuda')
         beta = torch.nn.Parameter(torch.tensor(1/9), requires_grad=True).to('cuda')
 
+        ## Fixing alpha is done in train.py by blocking the gradients over them
         w = torch.tensor([[alpha.clone(),  alpha.clone(), alpha.clone()],
                           [alpha.clone(),  beta, alpha.clone()],
                           [alpha.clone(),  alpha.clone(), alpha.clone()]])
@@ -59,52 +60,6 @@ class WN_GCN_layer(nn.Module):
                 in_channels = v
         return torch.nn.Sequential(*layer)
 
-#
-# class WN_GCN_layer_reverse(nn.Module):
-#     '''
-#     Implementation of WN-GCN
-#     '''
-#     def __init__(self, args, pointconv_cfg):
-#         super(WN_GCN_layer_reverse, self).__init__()
-#         self.aggregate = self.weighted_aggregation(pointconv_cfg[-1], pointconv_cfg[-1], 3)
-#         self.vertexfeat = self.make_layer(args, pointconv_cfg)
-#
-#     def forward(self, x):
-#         x = self.vertexfeat(x)
-#         x = self.aggregate(x)
-#
-#         return x
-#
-#     def weighted_aggregation(self, in_channel, out_channel, kernel_size):
-#
-#
-#         alpha = torch.nn.Parameter(torch.tensor(1/9), requires_grad=True).to('cuda')
-#         beta = torch.nn.Parameter(torch.tensor(1/9), requires_grad=True).to('cuda')
-#
-#         w = torch.tensor([[alpha.clone(),  alpha.clone(), alpha.clone()],
-#                           [alpha.clone(),  beta, alpha.clone()],
-#                           [alpha.clone(),  alpha.clone(), alpha.clone()]])
-#         w = w.view(1, 1, kernel_size, kernel_size).repeat(out_channel, 1, 1, 1)
-#         w = w.to('cuda')
-#         layer = torch.nn.Conv2d(in_channel, out_channel, kernel_size=kernel_size, padding=1, bias=False, groups=out_channel)
-#         layer.weight = torch.nn.Parameter(w, requires_grad=True)
-#         return layer
-#
-#
-#     def make_layer(self, args, pointconv_cfg):
-#         layer = []
-#         for i, v in enumerate(pointconv_cfg):
-#             if i == 0:
-#                 in_channels = v
-#             else:
-#                 try:
-#                     layer += [torch.nn.Conv2d(in_channels=in_channels, out_channels=v, kernel_size=1, bias=True), torch.nn.BatchNorm2d(v), torch.nn.ReLU()]
-#                 except (TypeError):
-#                     print("debug! come to GraphNetBlock ")
-#                     pdb.set_trace()
-#
-#                 in_channels = v
-#         return torch.nn.Sequential(*layer)
 
 
 class CosFaceClassifier(nn.Module):
@@ -156,33 +111,3 @@ class WN_GCN(nn.Module):
             if isinstance(v, list):
                 layer += [WN_GCN_layer(args, v, reverse)]
         return torch.nn.Sequential(*layer)
-#
-# class WN_GCN_reverse(nn.Module):
-#     '''
-#     Weighted nearest-Neighbor Graph Convolutional Network Module
-#     '''
-#
-#     def __init__(self, args, cfgs, cfgs_cls, cosface=True):
-#
-#         super(WN_GCN_reverse, self).__init__()
-#         self.encoder = self.make_layer(args, cfgs)
-#         self.GAP = torch.nn.AdaptiveAvgPool2d((1, 1))
-#         if cosface:
-#             self.classifier = CosFaceClassifier(cfgs_cls)
-#         else:
-#             self.classifier = nn.Sequential(nn.Linear(cfgs_cls[0], cfgs_cls[1]),
-#                                             nn.Linear(cfgs_cls[1], cfgs_cls[2]))
-#
-#     def forward(self, x):
-#         equi_feat = self.encoder(x)
-#         inv_feat = self.GAP(equi_feat)
-#         x = inv_feat.view(inv_feat.shape[0], -1)
-#         x = self.classifier(x)
-#         return x, equi_feat, inv_feat
-#
-#     def make_layer(self, args, cfgs):
-#         layer=[]
-#         for v in cfgs:
-#             if isinstance(v, list):
-#                 layer += [WN_GCN_layer_reverse(args, v)]
-#         return torch.nn.Sequential(*layer)
